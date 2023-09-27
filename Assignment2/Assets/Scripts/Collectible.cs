9044727bac6fbd7f6e7d79f5a59349e4a6f9f012
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-09-26 21:32:02 -0400

    This mostly works now. Now to edit the UI...

--- Assignment2/Assets/Scripts/Collectible.cs
+++ Assignment2/Assets/Scripts/Collectible.cs
@@ -4,11 +4,26 @@ public abstract class Collectible : MonoBehaviour
 {
     protected Color color;
     protected short scoreValue;
-    public Vector3 location = new Vector3(Random.Range(-100f, 100f), Random.Range(0.5f, 1f), Random.Range(-100f,100f));
+    protected string objectTag;
+    public Vector3 location = new Vector3(Random.Range(-100f, 100f), Random.Range(0.5f, 2.5f), Random.Range(-100f,100f));
     //public abstract void healthEffect();
     public short getScoreValue()
     {
         return scoreValue;
+    }
+    /*
+    public Vector3 getLocation()
+    {
+        return location;
+    }
+    */
+    public Color getColor()
+    {
+        return color;
+    }
+    public string getObjectTag()
+    {
+        return objectTag;
     }
 }
 
@@ -17,7 +32,8 @@ public class Coal : Collectible
     public Coal()
     {
         this.color = new Color(0, 0, 0);
-        this.scoreValue = 1;
+        this.objectTag = "Coal";
+        //this.scoreValue = 1;
     }
 }
 
@@ -26,6 +42,29 @@ public class Cherry : Collectible
     public Cherry()
     {
         this.color = new Color(255, 0, 0);
-        this.scoreValue = 3;
+        this.objectTag = "Cherry";
+        //this.scoreValue = 3;
     }
 }
+
+/*
+public abstract class Enemy
+{
+    protected short strength;
+    protected short speed;
+    public abstract void attack();
+}
+
+public class Lurker : Enemy
+{
+    public Lurker()
+    {
+        this.strength = 5;
+        this.speed = 3;
+    }
+    public override void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+*/
--- Assignment2/Assets/Scripts/GameManager.cs
+++ Assignment2/Assets/Scripts/GameManager.cs
@@ -6,10 +6,10 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     Vector3 vector;
-    const int numCollectibles = 2000;
+    const int numCollectibles = 100;
     public float timer = 0f;
     //private bool holdTimer = false; 
-    public float timerDuration = 60f;
+    public float timerDuration = 120f;
     public int score = 0;
     public float moveSpeed = 10f;
     public float rotateSpeed = 120f;
@@ -20,34 +20,17 @@ public class GameManager : MonoBehaviour
     public EventObserver eo;
     public GameObject coal;
     public GameObject cherry;
-    public Collectible sphere;
+    public GameObject sphere;
+    public GameObject newCollectible;
     public Collectible[] collectibles = new Collectible[numCollectibles];
+    Renderer renderer;
+    Collider m_ObjectCollider;
 
     public GameObject mainMusicObject;
     public AudioSource mainMusic;
 
     public TMP_Text coalText;
     public TMP_Text cherryText;
-
-    public abstract class Enemy
-    {
-        protected short strength;
-        protected short speed;
-        public abstract void attack();
-    }
-
-    public class Lurker : Enemy
-    {
-        public Lurker()
-        {
-            this.strength = 5;
-            this.speed = 3;
-        }
-        public override void attack()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -64,11 +47,29 @@ public class GameManager : MonoBehaviour
         for(i = 0; i < numCollectibles; ++i)
         {
             rando = Random.Range(1, 3);
+
+
             if (rando == 2)
+            {
                 collectibles[i] = new Cherry();
+                Debug.Log("Cherry created.");
+            }
             else
+            {
                 collectibles[i] = new Coal();
-            sphere = Instantiate(collectibles[i], collectibles[i].location, Quaternion.identity);
+                Debug.Log("Coal created.");
+            }
+            sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            m_ObjectCollider = sphere.GetComponent<Collider>();
+            m_ObjectCollider.isTrigger = true;
+            renderer = sphere.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", collectibles[i].getColor());
+            sphere.tag = collectibles[i].getObjectTag();
+            //Debug.Log(sphere.tag);
+            newCollectible = Instantiate(sphere, collectibles[i].location, Quaternion.identity);
+            newCollectible.AddComponent<CollectionNotifier>();
+
+
             /*
             vector = new Vector3(Random.Range(-100f, 100f), Random.Range(0.5f, 1f), Random.Range(-100f,100f));
 
@@ -78,21 +79,19 @@ public class GameManager : MonoBehaviour
                 sphere = Instantiate(cherry, vector, Quaternion.identity);
             */
 
+            /*
+            UnityEngine.MonoBehaviour:.ctor ()
+            Collectible:.ctor () (at Assets/Scripts/Collectible.cs:7)
+            Coal:.ctor () (at Assets/Scripts/Collectible.cs:25)
+            GameManager:Start () (at Assets/Scripts/GameManager.cs:57)
+            */
         }
+}
 
-        /*
-        for(i = 0; i < 500; ++i)
-        {
-            vector = new Vector3(Random.Range(-100f, 100f), Random.Range(0.01f, 1f), Random.Range(-100f,100f));
-            paper = Instantiate(failPaper, vector, Quaternion.identity);
-        }
-        */
-    }
-
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
         if(timer > 0)
-            timer -= Time.deltaTime;
+        timer -= Time.deltaTime;
     }
 }
